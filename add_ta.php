115b0613cbe9583dba3a553935aaebe4229eac27
vinay kumar agarwal <vka0797@gmail.com>
2019-04-17 18:02:54 +0530

    Grade assignment done on Faculty side

---
+++ add_ta.php
@@ -0,0 +1,98 @@
+<?php
+// Initialize the session
+
+session_start();
+require_once "config.php";
+$ta_id = $ta_id_err = "";
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || $_SESSION["logged_in_as"]=="System_Admin" || $_SESSION["logged_in_as"] == "Student"){
+    header("location: index.php");
+    exit;
+}
+else{
+	// Processing form data when form is submitted
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+     
+        // Validate TA ID
+        $offering_id = $_POST['offering_id'];
+        if(empty(trim($_POST['ta_id']))){
+            $ta_id_err = "Please enter the TA ID.";
+        } else{
+            // Prepare a select statement
+            $sql = "SELECT * FROM TA_Ship WHERE Offering_ID = ?";
+            
+            if($stmt = mysqli_prepare($link, $sql)){
+                // Bind variables to the prepared statement as parameters
+                mysqli_stmt_bind_param($stmt, "s", $param_ta_id);
+                
+                // Set parameters
+                $param_ta_id = trim($_POST["ta_id"]);
+                
+                // Attempt to execute the prepared statement
+                if(mysqli_stmt_execute($stmt)){
+                    /* store result */
+                    // mysqli_stmt_store_result($stmt);
+                    mysqli_stmt_store_result($stmt);
+                    if(mysqli_stmt_num_rows($stmt) == 1){
+                        $ta_id_err = "The TA already exists for this course";
+                    } else{
+                        $ta_id = trim($_POST["ta_id"]);
+                    }
+                } else{
+                    echo "Oops! Something went wrong. Please try again later.";
+                }
+            }
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+
+        if(empty($ta_id_err)){
+            
+            // Prepare an insert statement
+            $sql = "INSERT INTO TA_Ship values($offering_id, $ta_id)";
+            if(mysqli_query($link, $sql)){
+        		echo "TA successfully added.";
+        		header("location: faculty_mycourses.php");
+	        }
+	        else{
+	        	echo "Could not add a TA" . mysqli_error($link);
+	        }
+        }
+        mysqli_close($link);
+    }
+}
+?>
+
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Student Adding</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Add TA for this course</h2>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group <?php echo (!empty($ta_id_err)) ? 'has-error' : ''; ?>">
+                <label>TA ID</label>
+                <input type="text" name="ta_id" class="form-control" value="<?php echo $ta_id; ?>">
+                <span class="help-block"><?php echo $ta_id_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Submit">
+                <?php
+                $offering_id = $_GET['offering_id'];
+                echo "<input type='hidden' name='offering_id' value=$offering_id>";
+                ?>
+            </div>
+            <!-- <p>Already have an account? <a href="index.php">Login here</a>.</p> -->
+        </form>
+    </div>    
+</body>
+</html>
---
+++ ta_list.php
@@ -0,0 +1,77 @@
+<?php
+// Initialize the session
+
+session_start();
+require_once "config.php";
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || $_SESSION["logged_in_as"]=="System_Admin" || $_SESSION["logged_in_as"] == "Student"){
+    header("location: index.php");
+    exit;
+}
+?>
+
+<?php
+    if( isset( $_REQUEST['test'] ))
+    {
+        $student_id = $_REQUEST['student_id'];
+        $grade = $_REQUEST['grade'];
+        $offering_id = $_COOKIE['offering_id'];
+        $sql = "Update Enrollment set grade=$grade where Roll_Number=$student_id and Offering_ID=$offering_id";
+        if(mysqli_query($link, $sql)){
+        	echo "Grade Successfully submitted for ", $student_id;
+        }
+        else{
+        	echo "Could not submit grade" . mysqli_error($link);
+        }
+        
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; text-align: center; }
+        .wrapper{ width: 1250px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+    <h1> Assign the grades for</h1><br>
+    <?php
+        $offering_id = $_GET['offering_id'];
+        $sql = "Select * from TA_Ship where Offering_ID=$offering_id";
+        if($result = mysqli_query($link, $sql)){
+            if(mysqli_num_rows($result) > 0){
+                echo "<table class ='table table-hover'>";
+                echo "<tr>";
+                    echo "<th class = 'text-center'>Roll Number </th>";
+                    echo "<th class = 'text-center'>Name </th>";
+                echo "</tr>";
+                while($row = mysqli_fetch_array($result)){
+                    $roll_number = $row['Roll_Number'];
+                    $sql2 = "Select Name from Student where Roll_Number=$roll_number order by Roll_Number";
+                    $result_student = mysqli_query($link, $sql2);
+                    $row_student = mysqli_fetch_array($result_student);
+                    $name = $row_student['Name'];
+                    echo "<tr>";
+                        echo "<td class = 'text-center'>" . $roll_number. "</td>";
+                        echo "<td class = 'text-center'>" . $name . "</td>";
+                    echo "</tr>";
+                }
+                // Free result set
+                mysqli_free_result($result);
+            } else{
+                echo "No records matching your query were found.";
+            }
+        } else{
+            echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+        }   
+    ?>
+
+    </div>
+    <p> Go back to <a href="faculty_mycourses.php">My Courses</a>.</p>
+</body>
+</html>
